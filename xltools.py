e2a7014a307370189109fbef3b520c6232aacae9
Yannick Bahe <bahe@strw.leidenuniv.nl>
2020-06-14 14:12:34 +0200

    Convert indentation to spaces in xltools.py

--- xltools.py
+++ xltools.py
@@ -21,29 +21,29 @@ ops = {
 def connect_to_galaxies(bpart_ids, wdir, vr_snap, combined_vr=True):
     """Connect black holes to galaxies at a specified redshift.
 
-	Parameters
-	----------
-	bpart_ids : ndarray(int)
-	    The IDs of the black hole particle to match.
-	wdir : string
-		Directory of the simulation to work with
+    Parameters
+    ----------
+    bpart_ids : ndarray(int)
+        The IDs of the black hole particle to match.
+    wdir : string
+        Directory of the simulation to work with
     vr_snap : int or None
         Snapshot of the VR catalogue to connect to (None for no matching)
-	combined_vr : bool, optional
-		Flag for using transcribed VR, must currently be True.
+    combined_vr : bool, optional
+        Flag for using transcribed VR, must currently be True.
 
-	Returns
-	-------
-	gal_props : dict
-	    Dict with the results of the VR matching:
-	    - Haloes --> VR halo indices for each BH, -1 if unmatched
-	    - MStar --> Stellar mass of the host halo [M_Sun] within 30 pkpc,
-	                np.nan if not matched
-	    - SFR --> SFR of the host halo [M_Sun/yr] within 30 pkpc, np.nan
-	              if not matched
-	    - M200c --> M200c of the host halo [M_Sun], np.nan if not matched
-	    - HaloTypes --> Type index of the host halo, -1 if not matched
-		- Redshift --> Redshift of VR catalogue
+    Returns
+    -------
+    gal_props : dict
+        Dict with the results of the VR matching:
+        - Haloes --> VR halo indices for each BH, -1 if unmatched
+        - MStar --> Stellar mass of the host halo [M_Sun] within 30 pkpc,
+                    np.nan if not matched
+        - SFR --> SFR of the host halo [M_Sun/yr] within 30 pkpc, np.nan
+                  if not matched
+        - M200c --> M200c of the host halo [M_Sun], np.nan if not matched
+        - HaloTypes --> Type index of the host halo, -1 if not matched
+        - Redshift --> Redshift of VR catalogue
     """
     num_bhs = len(bpart_ids)
     gal_props = {}
@@ -54,7 +54,7 @@ def connect_to_galaxies(bpart_ids, wdir, vr_snap, combined_vr=True):
 
     if combined_vr:
         vr_particle_file = (f'{args.wdir}{args.vr_file}_'
-        	                f'{args.vr_snap:04d}_particles.hdf5')
+                            f'{args.vr_snap:04d}_particles.hdf5')
         vr_main_file = f'{args.wdir}{args.vr_file}_{args.vr_snap:04d}.hdf5'
     else:
         print("Please transcribe VR catalogue...")
@@ -62,16 +62,16 @@ def connect_to_galaxies(bpart_ids, wdir, vr_snap, combined_vr=True):
 
     # Also abort if VR catalogue could not be found
     if ((not os.path.isfile(vr_particle_file)) or
-    	(not os.path.isfile(vr_main_file))):
+        (not os.path.isfile(vr_main_file))):
         print(f"VR catalogue for snapshot {vr_snap} does not exist...")
         return None
 
     # Find redshift of VR catalogue
     aexp = float(hd.read_attribute(vr_main_file, 'SimulationInfo',
-    	                           'ScaleFactor'))
+                                   'ScaleFactor'))
     gal_props['Redshift'] = 1/aexp - 1
     print(f"Connecting to VR snapshot {vr_snap} at redshift "
-    	  f"{gal_props['Redshift']}...")
+          f"{gal_props['Redshift']}...")
     
     # Load VR particle IDs
     vr_ids = hd.read_data(vr_particle_file, 'Haloes/IDs')
@@ -104,7 +104,7 @@ def connect_to_galaxies(bpart_ids, wdir, vr_snap, combined_vr=True):
     ind_in_halo = found_in_vr[ind_good]
 
     vr_mstar = hd.read_data(args.vr_main_file,
-    	                    'ApertureMeasurements/30kpc/Stars/Masses')
+                            'ApertureMeasurements/30kpc/Stars/Masses')
     vr_sfr = hd.read_data(args.vr_main_file, 'ApertureMeasurements/30kpc/SFR/')
     vr_m200c = hd.read_data(args.vr_main_file, 'M200crit')
     vr_haloTypes = hd.read_data(args.vr_main_file, 'StructureTypes')
@@ -147,7 +147,7 @@ def get_sim_dir(base_dir, isim):
         # Make sure nothing stupid has happened
         if len(dirs) != 1:
             print(f"Could not unambiguously find directory for simulation "
-            	  f"{args.sim}!")
+                  f"{args.sim}!")
             set_trace()
         wdir = dirs[0]
 
@@ -164,32 +164,32 @@ def get_sim_dir(base_dir, isim):
 def get_all_sims(base_dir, base_name=None):
     """Find all simulations in a directory.
 
-	Parameters
-	----------
-	base_dir : str
-	    The directory in which to search for simulation runs.
-	base_name : str or None
-	    A string with which all simulations start. If None (default),
-	    all subdirectories are returned. NOT CURRENTLY IMPLEMENTED.
+    Parameters
+    ----------
+    base_dir : str
+        The directory in which to search for simulation runs.
+    base_name : str or None
+        A string with which all simulations start. If None (default),
+        all subdirectories are returned. NOT CURRENTLY IMPLEMENTED.
 
-	Returns
-	-------
-	simulations : list of str
-	    A list containing the (full) paths of all matching subdirectories
+    Returns
+    -------
+    simulations : list of str
+        A list containing the (full) paths of all matching subdirectories
 
     """
-	return [f.path for f in os.scandir(base_dir) if f.is_dir()]
+    return [f.path for f in os.scandir(base_dir) if f.is_dir()]
 
 
 def swift_Planck_cosmology():
-	"""Construct a flat FLRW cosmology that is, emprically, close to what
-	   Swift uses as its 'Planck13' cosmology."""
-	H0 = 67.79
-	h = H0/100
-	Oc0 = 0.1187/h**2
-	Ob0 = 0.02214/h**2
-	Om0 = Oc0 + Ob0
-	return FlatLambdaCDM(H0=H0, Om0=Om0, Ob0=Ob0)
+    """Construct a flat FLRW cosmology that is, emprically, close to what
+       Swift uses as its 'Planck13' cosmology."""
+    H0 = 67.79
+    h = H0/100
+    Oc0 = 0.1187/h**2
+    Ob0 = 0.02214/h**2
+    Om0 = Oc0 + Ob0
+    return FlatLambdaCDM(H0=H0, Om0=Om0, Ob0=Ob0)
 
 
 def lookup_bh_data(bh_data_file, bh_props_list, selection_list=None):
@@ -207,27 +207,27 @@ def lookup_bh_data(bh_data_file, bh_props_list, selection_list=None):
     # Now find the list of BHs we are interested in (based on z=0 props)
 
     if selection_list is None:
-    	sel = None
+        sel = None
 
     else:
-    	if num_bhs is None:
-    		# Did not load any properties, so cannot select anything. Doh.
-    		print("Cannot select any BHs, because we did not load any data.")
-    		set_trace()
+        if num_bhs is None:
+            # Did not load any properties, so cannot select anything. Doh.
+            print("Cannot select any BHs, because we did not load any data.")
+            set_trace()
 
-    	sel = np.arange(num_bhs, dtype=int)
-    	for selector in selection_list:
-    		
-    		# Apply requested comparison operation
-    		cmp = ops[selector[1]]
-    		ind_subsel = np.nonzero(cmp(bh_data[selector[0]][sel],
-    			                        selector[2]))[0]
+        sel = np.arange(num_bhs, dtype=int)
+        for selector in selection_list:
+            
+            # Apply requested comparison operation
+            cmp = ops[selector[1]]
+            ind_subsel = np.nonzero(cmp(bh_data[selector[0]][sel],
+                                        selector[2]))[0]
 
-    		# Adjust selection list
-    		sel = sel[ind_subsel]
+            # Adjust selection list
+            sel = sel[ind_subsel]
 
-	    # Sort BHs by index
-	    sel = np.sort(sel)
-	    print(f"There are {len(sel)} BHs in selection list.")
+        # Sort BHs by index
+        sel = np.sort(sel)
+        print(f"There are {len(sel)} BHs in selection list.")
 
     return bh_data, sel
