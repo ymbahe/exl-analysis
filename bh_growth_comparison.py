248e25168cdf241877ca0787287e594f1d47e89d
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2020-06-14 14:20:35 +0100

    Finished bh_growth_comparison.py

--- bh_growth_comparison.py
+++ bh_growth_comparison.py
@@ -31,32 +31,38 @@ def main():
     parser.add_argument('sim', help='Simulation index/name to analyse')
     parser.add_argument('--bh_bid', type=int,
                         help='BID of the black hole to highlight.')
-    parser.add_argument('--base_dir',
+    parser.add_argument('--base_dir', default=local.BASE_DIR,
                         help='Base directory of the simulation, default: '
                              f'{local.BASE_DIR}')
-    parser.add_argument('--bh_data_file',
+    parser.add_argument('--bh_data_file', default='black_hole_data.hdf5',
                         help='Name of the file containing the BH data, '
                              'default: "black_hole_data.hdf5"')
     parser.add_argument('--plot_prefix', default='gallery/bh_growth_tracks',
                         help='Prefix of output files, default: '
                              '"gallery/bh_growth_tracks')
-    parser.add_argument('--bh_mass_range', type=int, nargs='+',
+    parser.add_argument('--plot_mass_range', type=float, nargs='+',
                         help='Min and max mass of plot range, default: '
                              '5.0 8.5', default=[5.0, 8.5])
-    parser.add_argument('--most_massive_only', action='store_true',
+    parser.add_argument('--bh_mass_range', type=float, nargs='+',
+                        help='Min and max BH mass for selection, at target z '
+                             '[M_Sun], default: no selection.')
+    parser.add_argument('--bh_selection_redshift', type=float, default=0.0,
+                        help='Redshift at which to make BH mass selection '
+                             '(default: 0.0)')
+    parser.add_argument('--include_subdominant_bhs', action='store_true',
                         help='Only show black holes that are the most massive '
                              'in their galaxy, at the linked snapshot.')
-    parser.add_argument('--halo_mstar_range', default=[10.5, 11.4],
-                        help='Min and max stellar mass of host galaxy, '
-                             'default: 10.5 11.4'),
-    parser.add_argument('--halo_centrals_only', action='store_true',
+    parser.add_argument('--halo_mstar_range', default=[3e10, 4e11], type=float,
+                        help='Min and max stellar mass of host galaxy '
+                             '[M_Sun], default: 3e10, 4e11.')
+    parser.add_argument('--include_satellites', action='store_true',
                         help='Only show BHs in central haloes, not satellites.')
     parser.add_argument('--show_target_only', action='store_true',
                         help='Show only the target BH, not possible others '
                              'that also match the selection criteria.')
     parser.add_argument('--show_median', action='store_true',
                         help='Add the median for all selected BHs.')
-    parser.add_argument('--alpha_others', type=int, default=0.25,
+    parser.add_argument('--alpha_others', type=float, default=0.25,
                         help='Alpha value for non-target BHs, default: 0.25')
 
     args = parser.parse_args()
@@ -68,31 +74,45 @@ def main():
 
     # Find BHs we are intereste in, load data
     select_list = [
-        ["Halo_MStar", '>=', halo_mstar_range[0]],
-        ["Halo_Mstar", '<', halo_mstar_range[1]],
+        ["Halo_MStar", '>=', args.halo_mstar_range[0]],
+        ["Halo_MStar", '<', args.halo_mstar_range[1]],
     ]
-    if args.most_massive_only:
+    if not args.include_subdominant_bhs:
         select_list.append(['Flag_MostMassiveInHalo', '==', 1])
-    if args.halo_centrals_only:
+    if not args.include_satellites:
         select_list.append(['HaloTypes', '==', 10])
+    if args.bh_mass_range is not None:
 
+        zreds = hd.read_data(args.wdir + args.bh_data_file, 'Redshifts')
+        best_index = np.argmin(np.abs(zreds - args.bh_selection_redshift))
+        print(f"Best index for redshift {args.bh_selection_redshift} is "
+              f"{best_index}.")
+        
+        # Subgrid masses are in 10^10 M_sun, so need to adjust selection range
+        select_list.append(
+            ['SubgridMasses', '>=', args.bh_mass_range[0]/1e10, best_index])
+        select_list.append(
+            ['SubgridMasses', '<=', args.bh_mass_range[1]/1e10, best_index])        
+        
     bh_data, bh_list = xl.lookup_bh_data(args.wdir + args.bh_data_file,
                                          bh_props_list, select_list)
 
-    generate_track_image(args, bh_data)
+    generate_track_image(args, bh_data, bh_list)
     for iibh, ibh in enumerate(bh_list):
-        generate_track_image(args, bh_data, iibh)
+        generate_track_image(args, bh_data, bh_list, iibh)
 
+    print("Done!")
 
+    
 def generate_track_image(args, bh_data, bh_list, iibh=None):
     """Generate the track image, optionally highlighting one particular BH.""" 
 
     fig = plt.figure(figsize=(5.0, 4.5))
 
-    xr = [0, 14.0]   # Time axis range
+    xr = [0, cosmo.age(0).value]   # Time axis range
     xlabel = r'Time [Gyr]'
 
-    yr = args.bh_mass_range
+    yr = args.plot_mass_range
     ylabel = (r'$\log_{10}\,(M_\mathrm{BH,\, subgrid}\,/\,'
               r'\mathrm{M}_\odot)$')
 
@@ -107,25 +127,29 @@ def generate_track_image(args, bh_data, bh_list, iibh=None):
 
     times = bh_data['Times']
     log_masses = np.log10(bh_data['SubgridMasses']) + 10.0
-    halo_mstar = np.log10(bh_data['Halo_MStar']) + 10.0
 
+    if 'Halo_MStar' in bh_data and bh_data['Halo_MStar'] is not None:
+        halo_logmstar = np.log10(bh_data['Halo_MStar'])
+    else:
+        halo_logmstar = None
+        
     # Plot on the individual BH tracks                        
-    for ixbh in bh_list:
-        if halo_mstar is not None:
+    for iixbh, ixbh in enumerate(bh_list):
+        if halo_logmstar is not None:
             col = plt.cm.viridis(
-                (halo_mstar[ixbh] - np.log10(args.halo_mstar_range[0]))
+                (halo_logmstar[ixbh] - np.log10(args.halo_mstar_range[0]))
                 / np.log10(args.halo_mstar_range[1] / args.halo_mstar_range[0])
                 )
         else:
             col = 'black'
                     
-        if iibh is not None and ixbh == iibh:
+        if iibh is not None and iixbh == iibh:
             plot_alpha = 1.0
         else:
             plot_alpha = args.alpha_others
                     
-        plt.plot(times, log_masses[ibh, :], linewidth = 1.0,
-                 linestyle = isim[4], alpha=plot_alpha, color=col)
+        plt.plot(times, log_masses[ixbh, :], linewidth = 1.0,
+                 linestyle = '-', alpha=plot_alpha, color=col)
 
         if args.show_median:
             plot_average_profile(times, log_masses[sel, :].T, color=col,
@@ -142,11 +166,10 @@ def generate_track_image(args, bh_data, bh_list, iibh=None):
         plotloc = f'{args.wdir}{args.plot_prefix}_BH-all.png'
     else:
         plotloc = f'{args.wdir}{args.plot_prefix}_BH-BID-{bh_list[iibh]}.png'       
-    plt.savefig(plotloc_bh, dpi = 200, transparent = False)
-
-
-print("Done!")
-
+    plt.savefig(plotloc, dpi = 200, transparent = False)
+    plt.close('all')
+    
+    
 def get_redshift_axis(ax, xr, yr):
     """Create a second axis on the top bar for redshift labelling"""
 
