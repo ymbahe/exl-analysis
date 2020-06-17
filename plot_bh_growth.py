3cfe879096363751f423c3097d0642f62e39a565
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2020-06-17 20:52:33 +0100

    Adjustments to plot_bh_growth.py

--- plot_bh_growth.py
+++ plot_bh_growth.py
@@ -26,29 +26,29 @@ bh_props_list = ['Times', 'ParticleIDs',
                  'Velocities', 'GasRelativeVelocities',
                  'GasCircularVelocities', 'GasSoundSpeeds',
                  'GasVelocityDispersions', 'GasDensities', 'NumberOfTimeSteps',
-                 'NumberOfRepositionings', 'NumberOfRepositionAttempts',
+                 'NumberOfRepositions', 'NumberOfRepositionAttempts',
                  'NumberOfSwallows', 'NumberOfMergers', 'ViscosityFactors']
 
 panels = ['Masses', 'AccretionRate', 'Speeds', 'Density',
-          'Repositionings', 'RepositioningFractions']
+          'Repositions', 'RepositionFractions']
 
 yranges = {'Masses': [4.8, 8.0],
-           'AccretionRates': [-8.9, 0],
-           'ViscosityFactors': [-4, 0.05],
+           'AccretionRate': [-8.9, 0],
+           'ViscosityFactor': [-4, 0.05],
            'Speeds': [-0.9, 3.0],
            'Density': [4.0, 9.0],
-           'Repositionings': [0, 100],
-           'RepositioningFractions': [0, 1]}
+           'Repositions': [0, 100],
+           'RepositionFractions': [-0.02, 1.02]}
 
-xlabel = r'Time [Myr]'
+xlabel = r'Time [Gyr]'
 ylabels = {'Masses': r'BH masses (log $m$ [M$_\odot$])',
-           'AccretionRates': 'Accretion rates' '\n'
+           'AccretionRate': 'Accretion rates' '\n'
                              r'(log $\dot{m}$ [M$_\odot$/yr])',
-           'ViscosityFactors': r'log Viscosity Factors',
+           'ViscosityFactor': r'$\log_{10}$ Viscosity factor',
            'Speeds': r'Gas speeds (log $v$ [km/s])',
            'Density': r'Gas density (log)',
-           'Repositionings': r'Reposition numbers',
-           'RepositioningFractions': r'Reposition fractions'}
+           'Repositions': r'Reposition numbers',
+           'RepositionFractions': r'Reposition fractions'}
 
 # Plot the absolute velocity of the BH (w.r.t. the simulation frame)?
 plot_bvel = False
@@ -57,13 +57,15 @@ plot_bvel = False
 plot_bcvel = True
 
 def main():
+
+    argvals = None
     args = get_args(argvals)
 
     # Follow immediate instructions from args
-    mpl.rcParams['lines.linewidth'] = args.linewidth
+    mpl.rcParams['lines.linewidth'] = args.plot_linewidth
 
     if args.plot_mass_range is not None:
-        yranges['Masses'] = np.log10(np.array(args.plot_mass_range))
+        yranges['Masses'] = args.plot_mass_range
 
     if args.sims[0].lower() == 'all':
 
@@ -90,10 +92,10 @@ def process_sim(isim, args):
         args.wdir = isim
     else:
         args.wdir = xl.get_sim_dir(args.base_dir, isim)
-    print(f"Analysing simulation {wdir}...")
+    print(f"Analysing simulation {args.wdir}...")
 
     # Name of the input catalogue, containing all the data to plot
-    args.catloc = f'{wdir}{args.bh_file}'
+    args.catloc = f'{args.wdir}{args.bh_file}'
 
     # Select BHs in this sim
     if args.bh_bid is not None:
@@ -109,7 +111,7 @@ def process_sim(isim, args):
             select_list.append(['HaloTypes', '==', 10])
 
         if args.bh_mass_range is not None:
-            zreds = hd.read_data(wdir + args.bh_file, 'Redshifts')
+            zreds = hd.read_data(args.wdir + args.bh_file, 'Redshifts')
             best_index = np.argmin(np.abs(zreds - args.bh_selection_redshift))
             print(f"Best index for redshift {args.bh_selection_redshift} is "
                   f"{best_index}.")
@@ -124,13 +126,13 @@ def process_sim(isim, args):
     else:
         select_list = None
 
-    bh_file = wdir + args.bh_file
+    bh_file = args.wdir + args.bh_file
     bh_data, bh_list = xl.lookup_bh_data(bh_file, bh_props_list, select_list)
     args.nsnap = len(bh_data['Times'])
 
     # Overwrite selection with input, if specific BID(s) provided
     if args.bh_bid:
-        bh_list = [args.bh_bid]    
+        bh_list = args.bh_bid    
 
     # Extract meta-data from Header
     bh_data['CodeBranch'] = hd.read_attribute(bh_file, 'Code', 'Git Branch')
@@ -139,14 +141,14 @@ def process_sim(isim, args):
     bh_data['SimName'] = hd.read_attribute(bh_file, 'Header', 'RunName')
 
     for ibh in bh_list:
-        process_bh(args, ibh, bh_data)
+        process_bh(args, bh_data, ibh, isim)
 
-
+        
 def process_bh(args, bh_data, ibh, isim):
     """Process one BH from one simulation."""
 
-    fig = plt.figure(figsize=(args.width, 15))
-    
+    fig = plt.figure(figsize=(args.plot_width, 15))
+
     ind_good = np.nonzero((bh_data['Times'] >= 0) & 
                           (bh_data['SubgridMasses'][ibh, :] * 0 == 0) & 
                           (bh_data['SubgridMasses'][ibh, :] > 0))[0]
@@ -164,18 +166,22 @@ def process_bh(args, bh_data, ibh, isim):
     plot_config['tr'] = t_range
     plot_config['events'] = event_times
 
-    yranges['Repositionings'][1] = np.max(bh_data['NumberOfRepositionAttempts'])
+    yranges['Repositions'][1] = np.nanmax(
+        bh_data['NumberOfRepositionAttempts'][ibh, :])
 
     # Actual plots
-    for iipanel, ipanel in panels:
+    for iipanel, ipanel in enumerate(panels):
         plot_bh_panel(args, bh_data, plot_config, iipanel)
 
-    plt.subplots_adjust(left=0.2/(args.width/9),
-                    right=max(0.93/(args.width/9), 0.93),
+    plt.subplots_adjust(left=0.2/(args.plot_width/9),
+                    right=max(0.93/(args.plot_width/9), 0.93),
                     bottom=0.05, top=0.95, hspace=0, wspace=0)
 
     plt.show
-    plt.savefig(f'{args.plot_prefix}_BH{ibh}.pdf', dpi=200)
+    plotloc = f'{args.wdir}{args.plot_prefix}_BH-{ibh}.pdf'
+    if not os.path.isdir(os.path.dirname(plotloc)):
+        os.makedirs(os.path.dirname(plotloc))
+    plt.savefig(plotloc, dpi=200)
     plt.close('all')
 
 
@@ -189,14 +195,14 @@ def plot_bh_panel(args, bh_data, plot_config, iipanel):
     ax = plt.subplot(n_panels, 1, iipanel+1)
     ax.set_xlim(plot_config['tr'])
     ax.set_ylim(yranges[panel])
-    ax.set_ylabel(ylabels[panel]))
+    ax.set_ylabel(ylabels[panel])
     if iipanel < n_panels - 1:
         ax.axes.get_xaxis().set_visible(False)
     else:
         ax.set_xlabel(xlabel)
 
     # Mark times of repositions, swallows, mergers
-    draw_time_lines(plot_config['events'], yr)
+    draw_time_lines(plot_config['events'], yranges[panel])
 
     # Draw on actual evolution trends
     inds = plot_config['inds']
@@ -209,7 +215,8 @@ def plot_bh_panel(args, bh_data, plot_config, iipanel):
         plt.plot(times,
                  np.log10(bh_data['SubgridMasses'][ibh, inds] * conv_mass),
                  color='black')
-        plt.plot(times, np.log10(bh_data['Masses'][ibh, inds] * conv_mass),
+        plt.plot(times, np.log10(bh_data['DynamicalMasses'][ibh, inds]
+                                 * conv_mass),
                  color='black', linestyle=':')
 
         legend_item(ax, [0.03, 0.1], 0.94, 'Dynamical mass', alpha=0.5, ls=':')
@@ -217,7 +224,7 @@ def plot_bh_panel(args, bh_data, plot_config, iipanel):
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    elif panel == 'AccretionRates':
+    elif panel == 'AccretionRate':
         plt.plot(times,
                  np.log10(bh_data['AccretionRates'][ibh, inds] * conv_mdot),
                  color = 'black')
@@ -233,61 +240,63 @@ def plot_bh_panel(args, bh_data, plot_config, iipanel):
 
     elif panel == 'Speeds':
         vgas = np.log10(np.linalg.norm(bh_data['GasRelativeVelocities']
-                                              [ibh, :, inds]), axis=1)
+                                              [ibh, :, inds], axis=1))
         plt.plot(times, vgas, color='black')
         plt.plot(times, np.log10(bh_data['GasSoundSpeeds'][ibh, inds]),
                  color='goldenrod', linestyle=':')
 
-        legend_item(ax, [0.03, 0.1], 0.06, 'Gas sound speed', color='goldenrod',
-                    ls=':')
-        legend_item(ax, [0.03, 0.1], 0.2, 'Gas bulk speed')
+        legend_item(ax, [0.90, 0.97], 0.06, 'Gas sound speed', color='goldenrod',
+                    ls=':', text_side='left')
+        legend_item(ax, [0.90, 0.97], 0.2, 'Gas bulk speed', text_side='left')
 
         if plot_bvel:
             vbh = np.log10(np.linalg.norm(bh_data['Velocities'][ibh, :, inds],
                                           axis=1))
             plt.plot(times, vbh, color='black', linestyle = '--')
-            legend_item(ax, [0.03, 0.1], 0.13, 'BH velocity',
-                        color='cornflowerblue', ls='--')
+            legend_item(ax, [0.90, 0.97], 0.13, 'BH velocity',
+                        color='cornflowerblue', ls='--', text_side='left')
         elif plot_bcvel:
             vcgas = np.log10(np.linalg.norm(bh_data['GasCircularVelocities']
                                                    [ibh, :, inds], axis=1))
             plt.plot(times, vcgas, color='cornflowerblue', linestyle='--')
-            legend_item(ax, [0.03, 0.1], 0.13, 'Gas circular speed',
-                        color='cornflowerblue', ls='--')
+            legend_item(ax, [0.90, 0.97], 0.13, 'Gas circular speed',
+                        color='cornflowerblue', ls='--', text_side='left')
         elif bh_data['GasVelocityDispersions'] is not None:
             plt.plot(times, np.log10(bh_data['GasVelocityDispersion']
                                             [ibh, inds]),
                      color='cornflowerblue', linestyle='--')
-            legend_item(ax, [0.03, 0.1], 0.13, 'Gas velocity dispersion',
-                        color='cornflowerblue', ls='--')
+            legend_item(ax, [0.90, 0.97], 0.13, 'Gas velocity dispersion',
+                        color='cornflowerblue', ls='--', text_side='left')
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
     elif panel == 'Density':
         plt.plot(times, np.log10(bh_data['GasDensities'][ibh, inds]),
-                 color=black, linestyle='-')
+                 color='black', linestyle='-')
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    elif panel == 'Repositionings':
-        plt.plot(times, bh_data['NumberOfRepositionings'][ibh, inds],
+    elif panel == 'Repositions':
+        plt.plot(times, bh_data['NumberOfRepositions'][ibh, inds],
                  color='black')
         plt.plot(times, bh_data['NumberOfRepositionAttempts'][ibh, inds],
                  color='black', linestyle=':')
-        legend_item(ax, [0.03, 0.1], 0.06, 'Actual repositionings')
-        legend_item(ax, [0.03, 0.1], 0.13, 'Attempted repositionings',
-                    ls=':', alpha=0.5)
+        legend_item(ax, [0.90, 0.97], 0.06, 'Actual repositions',
+                    text_side='left')
+        legend_item(ax, [0.90, 0.97], 0.13, 'Attempted repositions',
+                    ls=':', alpha=0.5, text_side='left')
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    elif panel == 'RepositioningFractions':
+    elif panel == 'RepositionFractions':
         frac_repos, frac_att = get_repositioning_fractions(args, plot_config,
                                                            bh_data)
-        plt.plot(times[inds], frac_repos[inds], color='black')
-        plt.plot(times[inds], frac_att[inds], color='black', linestyle=':')
-        legend_item(ax, [0.03, 0.1], 0.06, 'Actual repositionings')
-        legend_item(ax, [0.03, 0.1], 0.06, 'Attempted repositionings',
-                    ls=':', alpha=0.5)
+        plt.plot(times, frac_repos[inds], color='black')
+        plt.plot(times, frac_att[inds], color='black', linestyle=':')
+        legend_item(ax, [0.90, 0.97], 0.06, 'Actual repositions',
+                    text_side='left')
+        legend_item(ax, [0.90, 0.97], 0.13, 'Attempted repositions',
+                    ls=':', alpha=0.5, text_side='left')
 
     # ---------------------------------------------------------------------
 
@@ -296,19 +305,21 @@ def plot_bh_panel(args, bh_data, plot_config, iipanel):
         axx = get_xaxis_redshift(ax)
 
         # Code info
-        legend_text(ax, 0.02, 0.08,
-            f"{bh_data['CodeBranch']} ({bh_data['CodeRev']})")
-        legend_text(ax, 0.02, 0.02, f"{bh_data['CodeDate']}")
+        legend_text(ax, 0.02, 0.03,
+            f"{bh_data['CodeBranch']} ({bh_data['CodeRev']}, "
+            f"{bh_data['CodeDate']})")
 
     elif iipanel == 1:
         legend_text(ax, 0.03, 0.03,
             f"BH {ibh} [ID = {bh_data['ParticleIDs'][ibh]}]")
 
     elif iipanel == 2:
-        legend_text(ax, 0.03, 0.03, f"{bh_data['SimName']}")
+        sim_name = bh_data['SimName'].replace('_', '\_')
+        legend_text(ax, 0.03, 0.03, f"{sim_name}")
 
 
-def legend_item(ax, xr, y, text, alpha=1, ls='-', color='black'):
+def legend_item(ax, xr, y, text, alpha=1, ls='-', color='black',
+                text_side='right'):
     """Add one legend item to the axes ax."""
 
     tr = ax.get_xlim()
@@ -317,9 +328,15 @@ def legend_item(ax, xr, y, text, alpha=1, ls='-', color='black'):
     plt.plot(tr[0] + (tr[1]-tr[0]) * np.array(xr),
              yr[0] + (yr[1]-yr[0]) * np.array([y, y]),
              linestyle=ls, color=color)
-    plt.text(tr[0] + (tr[1]-tr[0]) * (xr[1] + 0.02),
-             yr[0] + (yr[1]-yr[0]) * (y - 0.00),
-             text, va='center', ha='left', alpha=alpha, color=color)
+
+    if text_side == 'right':
+        text_x, text_ha = np.max(xr) + 0.02, 'left'
+    else:
+        text_x, text_ha = np.min(xr) -0.02, 'right'
+
+    plt.text(tr[0] + (tr[1]-tr[0]) * text_x,
+             yr[0] + (yr[1]-yr[0]) * y,
+             text, va='center', ha=text_ha, alpha=alpha, color=color)
 
 
 def legend_text(ax, x, y, text, alpha=1, color='black', fontsize=8):
@@ -330,7 +347,7 @@ def legend_text(ax, x, y, text, alpha=1, color='black', fontsize=8):
 
     plt.text(tr[0] + (tr[1]-tr[0]) * x,
              yr[0] + (yr[1]-yr[0]) * y,
-             text, va='center', ha='left', alpha=alpha, color=color,
+             text, va='bottom', ha='left', alpha=alpha, color=color,
              fontsize=fontsize)
 
 
@@ -340,9 +357,9 @@ def get_args(argv=None):
     print("Parsing input arguments...")
     parser = argparse.ArgumentParser(description="Parse input parameters.")
 
-    parser.add_argument('sim', nargs='+',
+    parser.add_argument('sims', nargs='+',
                         help='Simulation index/name to analyse ("all" to '
-                             'process all simulations in base_dir'.)
+                             'process all simulations in base_dir.')
     parser.add_argument('--base_dir', default=local.BASE_DIR,
                         help='Base directory of the simulation, default: '
                              f'{local.BASE_DIR}')
@@ -374,11 +391,11 @@ def get_args(argv=None):
                         help='Only show BHs in central haloes, not satellites.')
 
     parser.add_argument('--plot_mass_range', type=float, nargs='+',
-                        help='Min and max mass of plot range, default: '
+                        help='Min and max (log) mass of plot range, default: '
                              '5.0 8.5', default=[5.0, 8.5])
     parser.add_argument('--plot_width', type=float, default=9.0,
                         help='Plot width in inches (default: 9.0)')
-    parser.add_argument('--plot_linewidth', type=float, default:0.2,
+    parser.add_argument('--plot_linewidth', type=float, default=0.2,
                         help='Plot linewidth, default: 0.2')
 
     return parser.parse_args(argv)
@@ -400,20 +417,20 @@ def find_event_times(bh_data, ibh):
     ind_merger = np.nonzero((bh_data['NumberOfMergers'][ibh, 1:] >
                              bh_data['NumberOfMergers'][ibh, :-1]) &
                             (bh_data['NumberOfMergers'][ibh, 1:] > 0))[0]
-    if len(ind_mg) > 0:
+    if len(ind_merger) > 0:
         times_merger = bh_data['Times'][ind_merger]
     else:
         times_merger = None
     print(f"Total of {len(ind_merger)} BH mergers") 
 
-    ind_repos = np.nonzero((bh_data['NumberOfRepositionings'][ibh, 1:] >
-                            bh_data['NumberOfRepositionings'][ibh, :-1]) &
-                           (bh_data['NumberOfRepositionings'][ibh, 1:] > 0))[0]
+    ind_repos = np.nonzero((bh_data['NumberOfRepositions'][ibh, 1:] >
+                            bh_data['NumberOfRepositions'][ibh, :-1]) &
+                           (bh_data['NumberOfRepositions'][ibh, 1:] > 0))[0]
 
     if len(ind_repos) > 0:
-        time_repos = bh_data['Times'][ind_repos]
+        times_repos = bh_data['Times'][ind_repos]
     else:
-        time_repos = None
+        times_repos = None
     
     event_times = {'Swallows': times_swallow,
                    'Mergers': times_merger,
@@ -424,9 +441,9 @@ def find_event_times(bh_data, ibh):
 
 def draw_time_lines(event_times, yr):
     """Plot lines of swallows and reposition times"""
-    event_times = {}
     if event_times['Repositionings'] is not None:
-        for itime in event_times['Repositionings']:
+        for iitime, itime in enumerate(event_times['Repositionings']):
+            if iitime >= 10: break
             plt.plot([itime, itime], yr, color = 'black', alpha = 0.2,
                      zorder=-100)
     if event_times['Mergers'] is not None:
@@ -445,8 +462,8 @@ def get_yaxis_viscosity(ax):
     ax2 = ax.twinx()
 
     ax2.set_xlim(ax.get_xlim())
-    ax2.set_ylim(yranges['ViscosityFactors'])
-    ax2.set_ylabel(ylabels['ViscosityFactors'])
+    ax2.set_ylim(yranges['ViscosityFactor'])
+    ax2.set_ylabel(ylabels['ViscosityFactor'])
 
     ax2.spines['right'].set_color('purple')
     ax2.yaxis.label.set_color('purple')
@@ -493,7 +510,7 @@ def get_repositioning_fractions(args, plot_config, bh_data):
     # Work out the fraction of repositions and reposition attempts in
     # these ranges of outputs
     num_steps = bh_data['NumberOfTimeSteps']
-    num_repos = bh_data['NumberOfRepositionings']
+    num_repos = bh_data['NumberOfRepositions']
     num_att = bh_data['NumberOfRepositionAttempts']
     delta_steps = num_steps[ibh, end_ind] - num_steps[ibh, start_ind]
     delta_repos = num_repos[ibh, end_ind] - num_repos[ibh, start_ind]
