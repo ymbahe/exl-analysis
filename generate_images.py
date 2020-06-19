547fe5e1ebe03110cabb9b7fc2bf4fd877731e51
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2020-06-19 08:46:46 +0100

    Debugged generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -24,13 +24,9 @@ image_list = [(0, 'temp', '--draw_hsml'),
               (0, 'sfr', '--draw_hsml --scale -4.0 -0.5 --absscale'),
               (4, 'gri', '--nobh --absscale')]
 
-# List of snapshots for which to create images and websites
-snap_list = [24, 36]
-
-# Need to improve the following bit...
-snap_zred = np.zeros(37)
-snap_zred[24] = 1.08
-snap_zred[36] = 0.00
+vr_plots = [('M200c', 'MStar', 'sSFR'),
+            ('M200c', 'MBH', 'MStar'),
+            ('M200c', 'Size', 'MStar')]
 
 def main():
 
@@ -48,6 +44,13 @@ def main():
     parser.add_argument('--numpix', type=int,
                         help='Size of images in pixels, default: 1000',
                         default=1000)
+    parser.add_argument('--plot_prefix', default='gallery/vr-plots',
+                        help='Prefix for plot files, default: '
+                             'gallery/vr-plots')
+    parser.add_argument('--snapshots', nargs='+', type=int,
+                        help='Snapshots for which to set up websites.')
+    parser.add_argument('--snap_name', default='snapshot',
+                        help='Snapshot prefix, default: "snapshot".')
     parser.add_argument('--mstar_min', type=float,
                         help='Minimum stellar mass of the host galaxy for '
                              'a black hole to be included, in M_Sun '
@@ -71,6 +74,9 @@ def main():
 
     args = parser.parse_args()
 
+    if len(args.snapshots) == 0:
+        print("No snapshots selected, aborting.")
+    
     # Adjust selected front page size to closest match in list
     frontpage_sizeind = np.argmin(np.abs(np.array(ap_list)
                                          - args.size_frontpage))
@@ -78,17 +84,29 @@ def main():
 
     # Construct the full working directory of the simulation
     args.wdir = xl.get_sim_dir(args.base_dir, args.sim)
-    
-    # Find BHs we are intereste in, load data
-    select_list = [
-        ["Halo_M200c", '>=', args.m200_min],
-        ["Halo_MStar", '>=', args.mstar_min],
-        ["Flag_MostMassiveInHalo", '==', 1],
-        ["HaloTypes", '==', 10]
-    ]
-    bh_data, bh_list = xl.lookup_bh_data(args.wdir + args.bh_data_file,
-                                         bh_props_list, select_list)
 
+    # Look up the snapshot redshifts
+    get_snapshot_redshifts(args)
+    
+    args.plotdata_file = f'{args.wdir}{args.plot_prefix}.hdf5'
+    if os.path.isfile(args.plotdata_file):
+        bh_list = hd.read_data(args.plotdata_file, 'BlackHoleBIDs')
+        select_list = None
+    else:    
+        # Find BHs we are intereste in, load data
+        select_list = [
+            ["Halo_M200c", '>=', args.m200_min],
+            ["Halo_MStar", '>=', args.mstar_min],
+            ["Flag_MostMassiveInHalo", '==', 1],
+            ["HaloTypes", '==', 10]
+        ]
+        bh_list = None
+            
+    bh_data, bh_sel = xl.lookup_bh_data(args.wdir + args.bh_data_file,
+                                        bh_props_list, select_list)
+    if bh_list is None:
+        bh_list = bh_sel
+    
     if len(bh_list) == 0:
         print("No black holes selected, aborting.")
         return
@@ -103,6 +121,16 @@ def main():
     generate_website(args, bh_data, bh_list)
 
 
+def get_snapshot_redshifts(args):
+    """Look up the redshifts of all snapshots."""
+
+    args.snaps_zred = np.zeros(max(args.snapshots)+1) - 1
+    for isnap in args.snapshots:
+        snap_file = f'{args.wdir}{args.snap_name}_{isnap:04d}.hdf5'
+        args.snaps_zred[isnap] = hd.read_attribute(snap_file, 'Header',
+                                                   'Redshift')[0]
+
+
 def generate_image_script(args, bh_list):
     """Generate a script for auto-generation of images."""
 
@@ -111,7 +139,7 @@ def generate_image_script(args, bh_list):
     with open(f'{args.wdir}/image_script.sh', 'w') as writer:
         writer.write('#!/bin/tcsh\n\n')
 
-        for isnap in snap_list:
+        for isnap in args.snapshots:
             for ibh in bh_list:
                 for iap in ap_list:
                     for iim in image_list:
@@ -162,21 +190,22 @@ def generate_website(args, bh_data, bh_list):
                      '</head><body>')
 
         # Actual BH-specific sites are written one per snapshot.
-        for isnap in snap_list:
+        for isnap in args.snapshots:
 
             if isnap == args.snap_frontpage:
-                zind_bh_frontpage = np.argmin(
-                    np.abs(bh_data['Redshifts'] - snap_zred[isnap]))
-                print(f"BH output index for frontpage is {zind_bh_frontpage}.")
                 set_trace()
+                zind_bh_frontpage = np.argmin(
+                    np.abs(bh_data['Redshifts'] - args.snaps_zred[isnap]))
+                print(f"BH output index for frontpage is {zind_bh_frontpage}.")
+
             
             # Need to explicitly connect the BHs to the VR catalogue
             # at the current snapshot, not necessarily in BH catalogue itself.
             # Returns None if VR catalogue does not exist
-            vr_data = xl.connect_to_galaxies(bh_data['ParticleIDs'][bh_list],
-                                             args.wdir,
-                                             f'{args.vr_prefix}_{isnap:04d}')
-
+            vr_data = xl.connect_to_galaxies(
+                bh_data['ParticleIDs'][bh_list],
+                f'{args.wdir}{args.vr_prefix}_{isnap:04d}')
+               
             
             # Now process each BH in turn...
             for iibh, ibh in enumerate(bh_list):
@@ -212,8 +241,10 @@ def generate_website(args, bh_data, bh_list):
                           f'index_bh-{ibh}_snap-{isnap}.html', 'w'
                           ) as writer_bh:
 
-                    write_bh_header(writer_bh, ibh, isnap, bh_data, vr_bhdata)
-                    write_bh_plots(writer_bh, ibh, isnap, bh_list, vr_data)
+                    write_bh_header(writer_bh, ibh, isnap, bh_data, vr_bhdata,
+                                    args)
+                    write_bh_plots(writer_bh, ibh, isnap, bh_list,
+                                   args.plotdata_file)
                     write_bh_images(writer_bh, ibh, isnap)
                     
                     # Add html closing lines to BH site
@@ -250,7 +281,7 @@ def write_gallery_image(writer, ibh, isnap, size, m_subgrid, m200):
                  f'</div>\n')
 
     
-def write_bh_header(writer, ibh, isnap, bh_data, vr_bhdata):
+def write_bh_header(writer, ibh, isnap, bh_data, vr_bhdata, args):
     """Write the header for the BH site."""
 
     # Write HTML opening lines
@@ -264,9 +295,9 @@ def write_bh_header(writer, ibh, isnap, bh_data, vr_bhdata):
                  f'[ID={bh_data["ParticleIDs"][ibh]}]</h2>\n')
 
     writer.write(f'<h3> ')
-    for ixsnap in snap_list:
+    for ixsnap in args.snapshots:
         writer.write(f'<a href="index_bh-{ibh}_snap-{ixsnap}.html">'
-                     f'Snap {ixsnap} (z = {snap_zred[ixsnap]:.1f}) '
+                     f'Snap {ixsnap} (z = {args.snaps_zred[ixsnap]:.1f}) '
                      f'&nbsp; &nbsp; &nbsp;'
                      f'</a>')
     writer.write(f'</h3>')
@@ -282,44 +313,72 @@ def write_bh_header(writer, ibh, isnap, bh_data, vr_bhdata):
                      f'{vr_bhdata["log_sSFR"]:.3f} </h3>')
 
 
-def write_bh_plots(writer, ibh, isnap, bh_list, vr_data):
+def write_bh_plots(writer, ibh, isnap, bh_list, plotdata_file):
     """Write all the plots for a BH."""
 
+    # Black hole growth tracks
     bh_growth_im = f'bh_growth_tracks_BH-BID-{ibh}.png'
     writer.write(f'<a href="{bh_growth_im}">\n'
                  f'  <img src="{bh_growth_im}" alt="BH growth track" '
                  f'width=500>\n</a>\n')
 
-    # Can only do this if we have successfully linked to VR
-    if vr_data is None:
+    # Individual detailed growth chart for this BH
+    bh_chart_im = f'bh_evolution_BH-BID-{ibh}.png'
+    writer.write(f'<a href="{bh_chart_im}">\n'
+                 f'  <img src="{bh_chart_im}" alt="BH evolution" '
+                 f'width=500>\n</a>\n')
+
+    # Stellar birth densities
+    stellar_densities_im = f'stellar_densities_BH-BID-{ibh}.png'
+    writer.write(f'<a href="{stellar_densities_im}">\n'
+                 f'  <img src="{stellar_densities_im}" '
+                 f'alt="Stellar birth densities" '
+                 f'width=500>\n</a>\n')
+
+    writer.write(f'<br>')
+    
+    for iiplot, iplot in enumerate(vr_plots):
+        write_vr_plot(writer, ibh, isnap, bh_list, plotdata_file,
+                      iiplot, iplot)
+
+    writer.write(f'<br>')
+    
+
+def write_vr_plot(writer, ibh, isnap, bh_list, plotdata_file, iiplot, iplot):
+    """Write one single VR plot."""
+
+    plotloc = (f'vr-plots_{iplot[0]}-{iplot[1]}-{iplot[2]}_'
+               f'BH-{ibh}_snap-{isnap}.png')
+        
+    writer.write(f'<img src="{plotloc}" alt="{iplot[0]}-{iplot[1]}-'
+                 f'{iplot[2]}" ')
+    if os.path.isfile(plotdata_file):
+        writer.write(f'usemap="#map-{iplot[0]}-{iplot[1]}" ')
+    writer.write(f'width=450>\n')
+
+    # Interactive map is only possible if we have the data file
+    if not os.path.isfile(plotdata_file):
         return
 
-    gal_props_im = f'mstar-m200-ssfr_bh-bid-{ibh}_snap-{isnap}.png'
-    writer.write(f'<img src="{gal_props_im}" alt="Galaxy properties" '
-                 f'usemap="#workmap" width={500*5.5/5.0}>\n')
-
-    writer.write(f'<map name="workmap">\n')
+    writer.write('\n')
+    writer.write(f'<map name="map-{iplot[0]}-{iplot[1]}">')
+    writer.write('\n')
+    
+    imx = hd.read_data(plotdata_file, f'{iplot[0]}-{iplot[1]}/xpt')
+    imy = hd.read_data(plotdata_file, f'{iplot[0]}-{iplot[1]}/ypt')    
 
     # Add a link for each individual BH to the map...
     for ixbh, xbh in enumerate(bh_list):
 
-        if vr_data["M200c"][ixbh] <= 0 or vr_data["MStar"][ixbh] <= 0:
-            continue
-
-        imx = (np.log10(vr_data["M200c"][ixbh]) - 11.7) / (13.3-11.7)
-        imx = int((imx*0.67 + 0.15) * (500*5.5/5.0))
-
-        imy = (np.log10(vr_data["MStar"][ixbh]) - 10.4) / (11.3-10.4)
-        imy = (imy*0.8 + 0.15) * (500*4.5/5.0)
-        imy = int((500*4.5/5.0) - imy)
-
+        x_this = int(imx[ixbh] * 450)
+        y_this = int(imy[ixbh] * 450)
         rad = 5
                 
-        writer.write(f'<area shape="circle" coords="{imx}, {imy}, {rad}" '
-                     f'alt="BH {xbh}" href="index_bh-{xbh}_'
+        writer.write(f'<area shape="circle" coords="{x_this}, {y_this}, '
+                     f'{rad}" alt="BH {xbh}" href="index_bh-{xbh}_'
                      f'snap-{isnap}.html">\n')
 
-    writer.write(f'</map>\n<br>\n')
+    writer.write(f'</map>\n')
 
 
 def write_bh_images(writer, ibh, isnap):
